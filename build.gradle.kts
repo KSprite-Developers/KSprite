f7c0190bd8f7bd9566d9922ac4001e706564d43b
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-05-31 13:54:54 -0500

    add gradle stuff

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "io.github.ksprite"
+version = "1.0.0-alpha"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
